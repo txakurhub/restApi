600a84224f0ceafbb244c5b5d7230cac6e358264
Leandro Pereyra <me.leandrop@gmail.com>
2022-12-13 11:23:12 -0300

    auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/User";
 import jwt from "jsonwebtoken";
 import config from "../config";
+import Role from "../models/Role";
 
 export const signUp = async (req, res) => {
   const { username, email, password, roles } = req.body;
@@ -10,7 +11,17 @@ export const signUp = async (req, res) => {
     email,
     password: await User.encryptPassword(password),
   });
+
+  if (roles) {
+    const foundRoles = await Role.find({ name: { $in: roles } });
+    newUser.roles = foundRoles.map((r) => r._id);
+  } else {
+    const role = await Role.findOne({ name: "user" });
+    newUser.roles = [role._id.toString()];
+  }
+
   const savedUser = await newUser.save();
+  console.log(savedUser);
 
   const token = jwt.sign({ id: savedUser._id }, config.SECRET, {
     expiresIn: 86400,
