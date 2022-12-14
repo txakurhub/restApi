24da9ee2b427aa1dfdcbce668267df4ce38c3604
Leandro Pereyra <me.leandrop@gmail.com>
2022-12-14 17:53:00 -0300

    auth middleware

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -7,7 +7,7 @@ const router = Router();
 router.get("/", productController.getProducts);
 router.get("/:productId", productController.getProductById);
 router.post("/", verifyToken, productController.createProduct);
-router.put("/:productId", productController.updateProductById);
+router.put("/:productId", verifyToken, productController.updateProductById);
 router.delete("/:productId", productController.deleteProductById);
 
 export default router;
